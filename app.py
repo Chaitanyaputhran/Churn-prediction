142321e1317327c7de88d142d731b0d503c1df68
Chaitanyaputhran <21i07.chaitanya@sjec.ac.in>
2023-05-17 23:41:14 +0530

    added index.html

--- app.py
+++ app.py
@@ -1,59 +1,73 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, render_template, request
+import pandas as pd
 import pickle
-import numpy as np
-from flask import Flask, render_template
 
-app = Flask(__name__, template_folder='templates')
-app = Flask(__name__, static_folder='static')
-
-
-
+app = Flask(__name__)
 
 # Load the trained model
-try:
-    model = pickle.load(open('model.pkl', 'rb'))
-except EOFError:
-    print("Error: Unable to load the model from file. Please ensure that the file exists and is not empty.")
+with open('model.pkl', 'rb') as file:
+    model = pickle.load(file)
 
-
-# Create a Flask app
-
-
-# Define the home page route
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
-# Define the predict route
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Get the user input from the form
-    Partner = str(request.form['Partner'])
-    Dependents = str(request.form['Dependents'])
-    tenure= int(request.form['tenure'])
-    PhoneService = str(request.form[ 'PhoneService' ])
-    MultipleLines= str(request.form['MultipleLines'])
-    InternetService= str(request.form['InternetService'])
-    OnlineSecurity= str(request.form['OnlineSecurity'])
-    OnlineBackup= str(request.form['OnlineBackup'])
-    DeviceProtection= str(request.form['DeviceProtection'])
-    TechSupport= str(request.form['TechSupport'])
-    StreamingTV= str(request.form['StreamingTV'])
-    StreamingMovies= str(request.form['StreamingMovies'])
-    Contract= str(request.form['Contract'])
-    PaperlessBilling= str(request.form['PaperlessBilling'])
-    PaymentMethod= str(request.form['PaymentMethod'])
-    MonthlyCharges= float(request.form['MonthlyCharges'])
-    TotalCharges= float(request.form['TotalCharges'])
+    # Get user input from the form
+    partner = request.form['Partner']
+    dependents = request.form['Dependents']
+    tenure = int(request.form['tenure'])
+    phone_service = request.form['PhoneService']
+    multiple_lines = request.form['MultipleLines']
+    internet_service = request.form['InternetService']
+    online_security = request.form['OnlineSecurity']
+    online_backup = request.form['OnlineBackup']
+    device_protection = request.form['DeviceProtection']
+    tech_support = request.form['TechSupport']
+    streaming_tv = request.form['StreamingTV']
+    streaming_movies = request.form['StreamingMovies']
+    contract = request.form['Contract']
+    paperless_billing = request.form['PaperlessBilling']
+    payment_method = request.form['PaymentMethod']
+    monthly_charges = float(request.form['MonthlyCharges'])
+    total_charges = float(request.form['TotalCharges'])
 
-    # Create a numpy array with the user input values
-    input_data = np.array([[Partner, Dependents, tenure, PhoneService, MultipleLines, InternetService, OnlineSecurity, OnlineBackup, DeviceProtection, TechSupport, StreamingTV, StreamingMovies, Contract, PaperlessBilling, PaymentMethod , MonthlyCharges, TotalCharges]])
-    
-    # Use the trained model to make a prediction
-    prediction = model.predict(input_data)
-    
-    # Return the predicted risk level to the user
-    return render_template('result.html', prediction_text='THe churn is {}'.format(prediction[0]))
+    # Create a DataFrame with the user input
+    input_data = pd.DataFrame({
+        'Partner': [partner],
+        'Dependents': [dependents],
+        'tenure': [tenure],
+        'PhoneService': [phone_service],
+        'MultipleLines': [multiple_lines],
+        'InternetService': [internet_service],
+        'OnlineSecurity': [online_security],
+        'OnlineBackup': [online_backup],
+        'DeviceProtection': [device_protection],
+        'TechSupport': [tech_support],
+        'StreamingTV': [streaming_tv],
+        'StreamingMovies': [streaming_movies],
+        'Contract': [contract],
+        'PaperlessBilling': [paperless_billing],
+        'PaymentMethod': [payment_method],
+        'MonthlyCharges': [monthly_charges],
+        'TotalCharges': [total_charges]
+    })
+
+    # Preprocess the input data
+    cat_features = input_data.drop(['TotalCharges', 'MonthlyCharges', 'tenure'], axis=1)
+    cat_features = cat_features.apply(lambda col: le.transform(col.astype(str)))
+    num_features = input_data[['TotalCharges', 'MonthlyCharges', 'tenure']]
+    input_data_processed = pd.concat([num_features, cat_features], axis=1)
+
+    # Make prediction
+    churn_prediction = model.predict(input_data_processed)
+    if churn_prediction[0] == 1:
+        result = "Customer is likely to churn."
+    else:
+        result = "Customer is likely to stay."
+
+    return render_template('index.html', prediction=result)
 
 if __name__ == '__main__':
     app.run(debug=True)
